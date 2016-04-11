3ce1f3a8c4974720cb50458faf77034c2e334596
fterlid <fterlid@gmail.com>
2016-04-11 21:39:42 +0200

    Created initial component structure

--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,20 @@
 'use strict';
+import React from 'react';
 import ReactDom from 'react-dom';
+import Autocompleter from '../src/autocompleter';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Here be examples</h1>
+                <Autocompleter />
+            </div>
+        );
+    }
+}
 
 ReactDom.render(
-    <h1>Here be examples</h1>,
+    <App />,
     document.getElementById('content')
 );
---
+++ src/autocompleter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Autocompleter extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h2>Here be autocompletion</h2>
+        );
+    }
+}
+
+export default Autocompleter;
